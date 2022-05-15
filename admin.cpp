ef780e1de3088e71a5b90caae73036788f6208be
Griffin Smith <69931156+MrTechnical77@users.noreply.github.com>
2022-05-15 18:09:03 -0400

    Option to create new admin implemented

--- admin.cpp
+++ admin.cpp
@@ -5,6 +5,56 @@
 #include <string.h>
 #include <mysql/mysql.h>
 
+std::hash <std::string> passwordHash;
+
+// Function to create new admin account
+void createUser(MYSQL* connection){
+
+    std::string newUsername;
+    std::string newPassword;
+    std::stringstream usernameCheck;
+    MYSQL_RES *userRES;
+    
+    std::cout << "Creating new user..." << std::endl << std::endl;
+    std::cout << "Enter the new admin's USERNAME: ";
+    std::cin >> newUsername;
+
+    usernameCheck << "SELECT user FROM admin WHERE user = '" << newUsername << "';";
+    std::string usernameCheckString = usernameCheck.str();
+    mysql_query(connection, usernameCheckString.c_str());
+
+    userRES = mysql_store_result(connection);
+
+    if(mysql_num_rows(userRES) != 0){
+        std::cout << std::endl << "Username is taken, please try again." << std::endl << std::endl;
+        return;
+    }
+    
+    std::cout << std::endl << "Enter the new admin's PASSWORD: ";
+    std::cin >> newPassword;
+    std::cout << std::endl;
+
+    std::stringstream newUserInsert;
+
+    newUserInsert << "INSERT INTO admin(user, password) VALUES( '" << newUsername << "','" << passwordHash(newPassword) << "');";
+    std::string userInsertString = newUserInsert.str();
+    int insertErrno = mysql_query(connection, userInsertString.c_str());
+    if(insertErrno == 0){
+        std::cout << "New User Created Sucessfully!" << std::endl << std::endl;
+    } else {
+        std::cout << "Error creating new user, no changes made to database." << std::endl;
+    }
+
+    return;
+}
+
+void createEmployee(MYSQL* connection){
+
+
+
+    return;
+}
+
 
 int main(){
 
@@ -25,7 +75,6 @@ int main(){
 
     std::string adminUsername;
     std::string adminPassword;
-    std::hash <std::string> passwordHash;
     MYSQL_RES *userCheckRES;
     MYSQL_ROW userCheckROW;
 
@@ -60,9 +109,44 @@ int main(){
 
     if(passwordCheck == userCheckROW[2]){
         std::cout << "Login Success!" << std::endl << std::endl;
+        mysql_free_result(userCheckRES);
     } else {
         std::cout << "Incorrect Password" << std::endl;
+        mysql_free_result(userCheckRES);
         exit(1);
+    }
+
+    for(;;){
+        int userchoice = 0;
+        std::cout << "What operation would you like to perform?" << std::endl << std::endl;
+
+        std::cout << "1. Create New Admin Account" << std::endl;
+        std::cout << "2. Create New Employee" << std::endl;
+
+        std::cout << "0. Exit Admin Panel" << std::endl << std::endl;
+
+        std::cout << "Choice: ";
+        std::cin >> userchoice;
+        std::cout << std::endl << std::endl;
+
+        if(userchoice == 1){
+            createUser(connection);
+        }
+
+        if(userchoice == 2){
+            createEmployee(connection);
+        }
+
+        if(userchoice == 0){
+            std::cout << "Goodbye!" << std::endl << std::endl;
+            exit(1);
+        }
+
+        else{
+            std::cout << "Invalid Choice" << std::endl << std::endl;
+        }
+
+
     }
 
 
